af4d420fa0eaac484931a74ee6d81e2f6a7144e8
Fakhar  Khan <fakhar@softpyramid.com>
2015-10-14 19:37:09 +0500

    upgraded animations

--- resources/views/preview.blade.php
+++ resources/views/preview.blade.php
@@ -8,13 +8,15 @@
             <div> <span id="vader"></span><span id="luke" style="display: none;"></span></div>
         </div>
         <div class="row col-md-4" id="share" style="display: none;text-align: center">
-            <h2>Create your own <a href="{{url()}}">here</a></h2>
-            <h4>or</h4>
-            <div class="form-group">
-                <input   type="text" class="form-control" id="link" name="Link" value="{!! url($link->link) !!}">
+            <div id="shareBox">
+                <h2>Create your own <a href="{{url()}}">here</a></h2>
+                <h4>or</h4>
+                <div class="form-group">
+                    <input   type="text" class="form-control" id="link" name="Link" value="{!! url($link->link) !!}">
+                </div>
+                {{--<button  onclick="alert('how to share, johny?')" type="button" class="btn btn-info">Share</button>--}}
+                <share-button></share-button>
             </div>
-            {{--<button  onclick="alert('how to share, johny?')" type="button" class="btn btn-info">Share</button>--}}
-            <share-button></share-button>
         </div>
     </div>
 
@@ -30,34 +32,35 @@
             var theater = new TheaterJS();
 
             theater
-                    .describe("Vader", {speed: .8, accuracy: 1, invincibility: 4}, "#vader")
-                    .describe("Luke", {speed: .8, accuracy: 1, invincibility: 4}, "#luke")
+                    .describe("Vader", {speed: 0.6, accuracy: 1, invincibility: 4}, "#vader")
+                    .describe("Luke", {speed: 0.6, accuracy: 1, invincibility: 4}, "#luke")
 
             theater
-                    .write("Vader:{{ $link->vader }} ")
+                    .write("Vader:{{ $link->vader }}",600)
                     .write({
                         name: "call",
                         args: [
                             function () {
                                 var self = this;
-                                {{--@if(filter_var($link->luke, FILTER_VALIDATE_URL)==FALSE)--}}
                                     if(ValidURL("{{ $link->luke }}"))
                                     {
                                         window.location="{{ $link->luke }}";
                                     }
-                                {{--@else--}}
-                                    //Has no valid url
-                                {{--@endif--}}
 
-
-                                setTimeout(function () {
-                                    $('#vader').hide('slow',function()
-                                    {
-                                        $('#luke').show('slow');
+                                $( "#vader" ).fadeOut( 1000, function() {
+                                    $( "#luke" ).show('fast',function(){
                                         self.next();
-                                        console.log('going to second');
-                                    });
-                                }, 1000);
+                                    } );
+                                });
+
+//                                setTimeout(function () {
+//                                    $('#vader').hide('slow',function()
+//                                    {
+//                                        $('#luke').show();
+//                                        self.next();
+//                                        console.log('going to second');
+//                                    });
+//                                }, 1000);
                             },
                             true
                         ]
@@ -65,19 +68,18 @@
 
             //second scene
             theater
-                    .write("Luke:{{ $link->luke }} ")
+                    .write("Luke:{{ $link->luke }}",600)
                     .write({
                         name: "call",
                         args: [
                             function () {
-                                setTimeout(function () {
-                                    $('#luke').hide('slow',function(){
-                                        theater.stop();
-                                        $('#share').show('slow');
-                                        console.log('completed');
-                                    });
+                                //theater.stop();
+                                $( "#share" ).fadeIn( 3000, function() {
+                                    $( "#shareBox" ).fadeIn( 100 );
+                                });
 
-                                }, 1000);
+                               // $('#share').show(3000);
+                                console.log('completed');
                             },
                             true
                         ]
