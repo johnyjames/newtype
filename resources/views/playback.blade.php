c7ff8095f4a313ebe0edf8a1fa3d3fac81b2beb0
Fakhar  Khan <fakhar@softpyramid.com>
2015-10-16 17:01:02 +0500

    advance form

--- resources/views/playback.blade.php
+++ resources/views/playback.blade.php
@@ -27,55 +27,80 @@
         <label for="vader">First Part</label>
         <input type="text" class="form-control" id="recorder" name="recorder" placeholder="How are you today?">
     </div>
-
-    <p>Complete the typing and click outside the box</p>
+    <p>Complete the typing and click play button</p>
+    <button type="button" class="btn btn-lg btn-default" onclick="Play()">Play</button>
 @endsection
 
 @section('page-scripts')
     <script>
-        var Playback = {
-            //store the time an action occured and the resulting state in an object
-            //don't use an array because they are not sparce - interstitial keys
+        var Recording;
+        var Player = {
+            init: function(playbackId)
+            {
+                this.playback = document.getElementById( playbackId );
+            },
+
+            play: function(recording)
+            {
+                //store the time the sequence started
+                //so that we can subtract it from subsequent actions
+                var mark = null;
+                for( var t in  recording ) {
+                    if( mark ) {
+                        var timeout = t - mark;
+                    } else {
+                        var timeout = 0;
+                        mark = t;
+                    }
+                    // We need to create a callback which closes over the value of t
+                    // because t would have changed by the time this is run
+                    setTimeout( Player.changeValueCallback( recording[t] ), timeout );
+                }
+            },
+            changeValueCallback: function( val ) {
+                return function() { Player.playback.value = val }
+            }
+        };
+
+        var Recorder = {
+            //store the time an action occurred and the resulting state in an object
+            //don't use an array because they are not sparse - interstitial keys
             //will have to be iterated over
             record: {},
-            init: function( recorderId, playbackId ) {
+            init: function( recorderId) {
                 this.recorder = document.getElementById( recorderId );
-                this.playback = document.getElementById( playbackId );
 
                 this.recorder.addEventListener( 'focus', function() {
-                    Playback.record = {};
+                    Recorder.record = {};
                     this.value = '';
                 }, false );
 
                 this.recorder.addEventListener( 'keyup', function( e ) {
-                    Playback.record[ (new Date()).getTime() ] = this.value;
+                    Recorder.record[ (new Date()).getTime() ] = this.value;
                 }, false );
 
                 this.recorder.addEventListener( 'blur', function( e ) {
-                    Playback.playback.value = '';
-                    //store the time the sequence started
-                    //so that we can subtract it from subsequent actions
-                    var mark = null;
-                    for( var t in  Playback.record ) {
-                        if( mark ) {
-                            var timeout = t - mark;
-                        } else {
-                            var timeout = 0;
-                            mark = t;
-                        }
-                        // We need to create a callback which closes over the value of t
-                        // because t would have changed by the time this is run
-                        setTimeout( Playback.changeValueCallback( Playback.record[t] ), timeout );
-                    }
+                    Player.playback.value = '';
+                    Recording=JSON.stringify(Recorder.record);
+                    console.log('Recording stored');
                 }, false );
 
-            },
-
-            changeValueCallback: function( val ) {
-                return function() { Playback.playback.value = val }
             }
+
+        };
+
+        Recorder.init( 'recorder');
+        Player.init( 'playback');
+
+
+        function Play()
+        {
+            var data=JSON.parse(Recording);
+
+            Player.play(data);
+
+            //console.log(JSON.parse(Recording));
         }
 
-        Playback.init( 'recorder', 'playback' );
     </script>
 @endsection
